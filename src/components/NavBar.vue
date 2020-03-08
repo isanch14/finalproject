29011db07e6a50ebbe4406d59361a3c7669eabc7
isanch14 <isanch14@stu.southtexascollege.edu>
2020-03-07 21:39:51 -0600

    Clean App, Start Home Page

---
+++ src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav
+    class="nav w-screen z-20 text-black"
+    :class="ishome ? '' : 'shadow-2xl bg-red-800'"
+  >
+    <div class="left">
+      <span class="font-semibold"> 
+          Welcome!
+      </span>
+    </div>
+    <div class="right">
+      <router-link class="link" to="/">Home</router-link>
+      <router-link class="link" to="/About">About</router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {
+    ishome: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  @apply h-24 fixed px-12 flex justify-between items-center;
+}
+.link {
+  @apply px-2 font-semibold;
+  &:hover {
+    @apply text-purple-800;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <NavBar />
+    <div
+      class="relative h-screen w-screen bg-cover bg-center shadow-2xl"
+      :style="`background-image:url(${bgImage})`"
+    >
+      <div class="h-screen w-screen bg-red-800 opacity-75" />
+      <div
+      class="absolute h-screen w-screen top-0 flex flex-col justify-center items-center"
+      >
+        <h1
+          class="text-6xl font-bold text-black px-4 hover:bg-white hover:text-blue-800 opacity-75"
+        >
+          Poco Bueno Ranch
+        </h1>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import NavBar from '../components/NavBar'
+import bgImage from '../assets/horsebackground.jpg'
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld
+    NavBar
+  },
+  data() {
+    return {
+      bgImage
+    }
   }
-};
+}
 </script>
