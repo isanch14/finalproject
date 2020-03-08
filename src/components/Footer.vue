29b615188143ab2d2e894fc7358534a3b57e887f
isanch14 <isanch14@stu.southtexascollege.edu>
2020-03-08 16:50:45 -0500

    Made Changes to Prepare for Deploy

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,12 +2,7 @@
   <div class="flex justify-center items-center">
     <div class="h-20 bottom-o py-8 flex">
       <p class="pr-2">&copy; Poco Bueno Ranch</p>
-      <a
-        class="facebook"
-        href="https://www.facebook.com/profile.php?id=1808742291"
-        target="_blank"
-        ><Facebook
-      /></a>
+      <a class="facebook" href="#" target="_blank"><Facebook/></a>
       <a class="twitter" href="#" target="_blank"><Twitter /></a>
     </div>
   </div>
